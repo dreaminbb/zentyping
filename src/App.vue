31e8f0116d394445d2d103e49206247db9a94db8
dremain <144113506+dreaminbb@users.noreply.github.com>
2024-04-04 10:58:13 +0900

    added css animetion on center and right card

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const writting = 'おだいをかいてみよう'
+const displayText = ref('')
+
+onMounted(() => {
+  let index = 0
+  const intervalId = setInterval(() => {
+    if (index < writting.length) {
+      displayText.value += writting.charAt(index)
+      index++
+    } else {
+      index = 0
+      displayText.value = ''
+    }
+  }, 300) // 1秒ごとに文字を追加
+})
+</script>
 <template>
   <body>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Yuji+Hentaigana+Akari&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,-25"
+    />
     <header>
-      <h1>head</h1>
+      <h1>justy typing</h1>
     </header>
     <div class="card" id="left">
       <p id="rankingCaracter">Ranking</p>
@@ -10,16 +38,32 @@
     </div>
     <div class="card" id="center">
       <button class="button" id="writeButton">write</button>
+      <span id="writtingAnimetion">{{ displayText }}</span>
       <img src="..\iconandpic\paint-brush.png" alt="" id="writtingicon" />
     </div>
     <div class="card" id="right">
       <div class="card__content"></div>
+      <span class="material-symbols-outlined" id="keyboardicon"> keyboard </span>
       <button class="button" id="playButton">play</button>
     </div>
   </body>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Yuji+Hentaigana+Akebono&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Slackside+One&family=Yuji+Hentaigana+Akebono&display=swap');
+
+.yuji-hentaigana akebono-regular {
+  font-family: 'Yuji Hentaigana Akebono', cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.slackside-one-regular {
+  font-family: 'Slackside One', cursive;
+  font-weight: 400;
+  font-style: normal;
+}
 * {
   box-sizing: border-box;
   padding: 0;
@@ -45,14 +89,16 @@ header {
   top: 0%;
   position: absolute;
   display: flex;
-  background: linear-gradient(to right, rgb(22, 138, 167), rgb(250, 250, 250));
+  background: linear-gradient(to right, rgb(196, 101, 101), rgb(136, 111, 111));
   filter: brightness(120%); /* 光度を20%上げる */
-  color: #000000;
+
   border-radius: 10px;
 }
 h1 {
+  font-family: 'Yuji Hentaigana Akari', cursive;
   font-size: 3rem;
-  background-color: rgb(168, 106, 106) rgb(187, 149, 149) rgb(204, 188, 188);
+  color: #00eeff;
+  text-align: left;
 }
 
 .card {
@@ -103,8 +149,14 @@ h1 {
   border: 1px solid #ffffff;
   transition: 0.5s;
 }
+#checkButton:hover {
+  background: linear-gradient(144deg, #af40ff, #5b42f3 50%, #00ddeb);
+  box-shadow: 1px 30px 40px rgba(233, 233, 233, 0.1);
+  transform: translateY(-10px);
+}
 
 #center {
+  display: block;
   box-sizing: border-box;
   background: #ffffff;
   background-image: linear-gradient(#ffffff 1.1rem, #ccc 1.2rem);
@@ -113,9 +165,17 @@ h1 {
   cursor: move;
   transition: 0.5s;
 }
+#center:hover {
+  box-shadow: 10px 50px rgba(194, 194, 194, 0.1);
+  transition: 0.5s;
+  transform: translateY(-10px);
+  transform: rotate(10deg);
+}
 
 #writeButton {
   background-color: #e6e6e6;
+  position: absolute;
+  left: 35%;
 }
 #writeButton:hover {
   background-color: #a5a5a5;
@@ -124,13 +184,38 @@ h1 {
   transform: translateY(-10px);
 }
 #writtingicon {
+  padding: 0;
+  margin: 0;
   position: absolute;
   display: flex;
   color: #00ddeb;
   width: 10%;
   height: 9%;
   right: 10%;
-  bottom: 30%;
+  bottom: 60%;
+  animation: shake-diagonal 1s infinite;
+}
+@keyframes shake-diagonal {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(5deg);
+  }
+  50% {
+    transform: rotate(0deg);
+  }
+  75% {
+    transform: rotate(-5deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+#writtingAnimetion {
+  font-family: 'slackside one', cursive;
+  font-size: 3rem;
+  word-wrap: break-word;
 }
 
 #right {
@@ -147,15 +232,27 @@ h1 {
   width: 100%;
   height: 100%;
 }
+#right:hover {
+  transition: 0.7s;
+  filter: brightness(150%);
+  transform: translateY(10%);
+}
 #playButton {
   background: linear-gradient(144deg, #6f4d8a, #414294 60%, #11555a);
   border: 1px solid #ffffff;
   transition: 0.5s;
 }
-
+#keyboardicon {
+  position: absolute;
+  display: flex;
+  color: #00ddeb;
+  bottom: 40%;
+  font-size: 8rem;
+  color: #525b8a;
+}
 #playButton:hover {
   background: linear-gradient(144deg, #6f4d8a, #4245f3 40%, #db2222);
-  box-shadow:  1px 30px 40px rgba(233, 233, 233, 0.1);
+  box-shadow: 1px 30px 40px rgba(233, 233, 233, 0.1);
   transform: translateY(-10px);
 }
 </style>
