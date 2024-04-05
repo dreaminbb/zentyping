ba75dc312285b7cbcbb523ea3c50275f0f93d1e0
dremain <144113506+dreaminbb@users.noreply.github.com>
2024-04-05 12:45:28 +0900

    localserver dosent work well

--- src/Play.vue
+++ src/Play.vue
@@ -1,16 +1,27 @@
-<script setup>
-const hello = 'ducky'
+<script>
+import { ref, onMounted } from 'vue'
+
+const writting = 'おだいをかいてみよう'
+const displayText = ref('')
+onMounted(() => {
+  let index = 0
+  setInterval(() => {
+    if (index < writting.length) {
+      displayText.value += writting.charAt(index)
+      index++
+    } else {
+      index = 0
+      displayText.value = ''
+    }
+  }, 300) // 1秒ごとに文字を追加
+})
 </script>
 <template>
-  <body>
-    <div>
-      <h1>{{ hello }}</h1>
-    </div>
-  </body>
+  <div>
+    <h1>GamePlay</h1>
+  </div>
 </template>
-
 <style scoped>
-/* Your component-specific styles go here */
 h1 {
   color: red;
 }
