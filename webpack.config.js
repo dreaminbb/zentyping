b9102cf162aa1b65c3d94eba0a5ce723d97b6b4f
dremain <shinimpact.tnk@gmail.com>
2024-04-27 21:13:52 +0900

    名前を変えてtsを追加した

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-// const path = require('path')
-
-// module.exports = {
-//     entry: './src/main.js', // エントリーポイントのファイル
-//     output: {
-//         filename: 'main.js', // バンドル後のファイル名
-//         path: path.resolve(__dirname, 'dist') // バンドル後のファイルの出力先
-//     }
-// }
