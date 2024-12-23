ba388410ff2e77eebe9c2dffe8fac34670cc5aa1
dremain <shinimpact.tnk@gmail.com>
2024-12-23 13:57:19 +0900

    ヘッダー作成

---
+++ app/template/src/components/base_header.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import config from '@/config'
+
+export default defineComponent({
+  name: 'base_header',
+  setup() {
+    return {
+      config
+    }
+  }
+})
+</script>
+
+<template>
+  <header id="header_container">
+    <h1>
+      <router-link id='site_name' to="/">{{ config.site_name }} <i class="fa-solid fa-code"></i></router-link>
+    </h1>
+    <nav>
+      <div>
+        <router-link to="/ranking"><font-awesome-icon class="nav_icon1"  icon="crown" /></router-link>
+      </div>
+      <div>
+        <router-link to="/setting"><font-awesome-icon class="nav_icon2"  icon="gear" /></router-link>
+      </div>
+      <div>
+        <router-link to="/notic"><font-awesome-icon class="nav_icon3" icon="bell" /></router-link>
+      </div>
+      <div>
+        <router-link to="/profile"><font-awesome-icon class="nav_icon4" icon="user" /></router-link>
+      </div>
+    </nav>
+  </header>
+</template>
--- app/template/src/main.ts
+++ app/template/src/main.ts
@@ -5,42 +5,10 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faCircle,
-  faCircleXmark,
-  faCoffee,
-  faGear,
-  faList,
-  faPen,
-  faQuestion,
-  faRankingStar,
-  faTimes,
-  faUser,
-  faXmark,
-} from '@fortawesome/free-solid-svg-icons'
-
-import { faCrow } from '@fortawesome/free-solid-svg-icons/faCrow'
-import { faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons'
-import { session_manager } from '@/services/auth'
-import { user_info } from '@/store/store'
-
-library.add(
-  faCrow,
-  faCoffee,
-  faQuestion,
-  faUser,
-  faRankingStar,
-  faGear,
-  faPen,
-  faGithub,
-  faList,
-  faXTwitter,
-  faCircle,
-  faTimes,
-  faXmark,
-  faCircleXmark,
-)
+import { library, type IconPack, type IconFamily } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -80,11 +48,10 @@ const router = createRouter({
   routes
 })
 
-
+library.add(fas as IconPack, far as IconPack, fab as IconPack)
 
 const app = createApp(App)
 const pinia = createPinia()
-console.log('router is here')
 app.use(router)
 app.use(pinia)
 app.component('font-awesome-icon', FontAwesomeIcon)
@@ -93,51 +60,3 @@ app.mount('#app')
 export const play_api_key: string = import.meta.env.VITE_SERVER_API_KEY
 export const seacret_key: string = import.meta.env.VITE_SEND_PLAY_INFO_API_KEY
 export const github_oauth_url: string = 'http://localhost:8000/auth/github'
-
-//サイトにアクセスしたらセッションが有効かを確認
-// document.cookie ? new session_manager().verify_session() : void 0
-// document.cookie ? user_info().fetch_info() : void 0
-
-// プレイの処理でcompleted_play_countを加える
-
-//ユーザーのデーターを取得
-// if (document.cookie && user_status().is_login) {
-//   try {
-//     const response: Response = await fetch('http://localhost:8000/user/info', {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     })
-//     const data = await response.json()
-//     if (response.status === 200) {
-//       if (data) {
-//         // user_info().play_history_value = data['play_history'] as Array
-//         user_info().user_name = data['name'] as string
-//         user_info().bio = data['bio'] as string
-//         user_info().joined_day = data['created_at'].split('T')[0].replace(/-/g, ':') as string
-//         user_info().play_count = data['total_result']['play_count'] as number
-//         user_info().total_time = data['total_result']['total_time'] as number
-//         user_info().short_correct_rate = data['total_result']['short_correct_rate'] as number
-//         user_info().normal_correct_rate = data['total_result']['normal_correct_rate'] as number
-//         user_info().long_correct_rate = data['total_result']['long_correct_rate'] as number
-
-//       } else {
-//         console.log('you are bad boy')w
-//         user_info().user_name = '(:'
-//         user_info().total_time = 0
-//       }
-//     } else if (response.status === 401) {
-//       console.log('ログインし直してください')
-//     } else if (response.status === 500) {
-//       console.log('サーバー側でエラーが発生しました')
-//     }
-//   } catch (error) {
-//     console.error(error)
-//   }
-// } else {
-//   console.log('no cookie???')
-// }
-
-// 最後に離れたらサーバーに送信 window.addEventListener('beforeunload', (): void => { if (document.cookie) {
-//     navigator.sendBeacon('http://localhost:8`000/exit', document.cookie) /     return } else { return } })
