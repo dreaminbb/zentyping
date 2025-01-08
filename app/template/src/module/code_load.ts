b73cda10d97619bd170968f55de36902d12177ff
dremain <shinimpact.tnk@gmail.com>
2025-01-08 10:21:30 +0900

    ちいさな修正

--- app/template/src/module/code_load.ts
+++ app/template/src/module/code_load.ts
@@ -3,7 +3,6 @@ import { code_data } from '@/store/store'
 
 export function code_load(first_mount: boolean): void {
 
-<<<<<<< HEAD
   // If player change the code and start it before class 
   // play_func's essensed span dosen change so this cause a bug.
   // What's the bug?
@@ -14,27 +13,9 @@ export function code_load(first_mount: boolean): void {
   // If code is changed, rerender the play ui.
   const code: string = code_data().code_data_obj[
     code_data().code_lang as 'python' | 'rust' | 'typescript'
-  ]?.[code_data().code_point].code || '404'
+  ]?.[code_data().code_point]?.code || '404'
   //* wait until finish rendering
   setTimeout(() => {
     play_func_inc.init(code)
   },500)
-=======
-                // If player change the code and start it before class 
-                // play_func's essensed span dosen change so this cause a bug.
-                // What's the bug?
-                // The bug is essenced span dosen't change from before span.
-                // If before of code had 10 char and change the code it still 10 char.
-                // So I add this code to fix this bug.
-                // If code is changed, rerender the play ui.
-                play_func_ins.delete()
-                const code: string = code_data().code_data_obj[
-                                code_data().code_lang as 'python' | 'rust' | 'typescript'
-                ]?.[code_data().code_point]?.code || '404'
-                
-                console.log(code_data().code_point)
-                console.error(code)
-                setTimeout(() => {
->>>>>>> f29cb1b7ebf36e99f694efc67768fe81d1d34ba9
-
 }
