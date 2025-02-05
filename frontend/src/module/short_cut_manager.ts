e2b3a817a48754e5f962849059e88bfe33a0f830
dremain <shinimpact.tnk@gmail.com>
2025-02-05 11:04:11 +0900

    formatting

--- frontend/src/module/short_cut_manager.ts
+++ frontend/src/module/short_cut_manager.ts
@@ -2,26 +2,26 @@ import play_func_ins from "./play_func";
 
 class short_cut {
 
-                public short_cut_handler: (e: KeyboardEvent) => void;
+  public short_cut_handler: (e: KeyboardEvent) => void;
 
-                constructor() {
-                                this.short_cut_handler = this.handle_keydown_for_short_cut.bind(this);
-                }
+  constructor() {
+    this.short_cut_handler = this.handle_keydown_for_short_cut.bind(this);
+  }
 
-                public able_short_cut(): void {
-                                document.addEventListener('keydown', this.handle_keydown_for_short_cut);
-                }
+  public able_short_cut(): void {
+    document.addEventListener('keydown', this.handle_keydown_for_short_cut);
+  }
 
-                public disable_chort_cut(): void {
-                                document.removeEventListener('keydown', this.handle_keydown_for_short_cut);
-                }
+  public disable_chort_cut(): void {
+    document.removeEventListener('keydown', this.handle_keydown_for_short_cut);
+  }
 
-                public handle_keydown_for_short_cut(e: KeyboardEvent): void {
-                                if (e.key === 'Tab') {
-                                                play_func_ins.next_code();
-                                }
+  public handle_keydown_for_short_cut(e: KeyboardEvent): void {
+    if (e.key === 'Tab') {
+      play_func_ins.next_code();
+    }
 
-                }
+  }
 
 }
 
