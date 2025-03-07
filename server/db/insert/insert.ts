5bec3cfb03816f9435ea3537eda5f77ad48eb810
dremain <shinimpact.tnk@gmail.com>
2025-03-07 14:23:13 +0900

    front end file fix

--- server/db/insert/insert.ts
+++ server/db/insert/insert.ts
@@ -1,6 +1,7 @@
 import { Collection, MongoClient } from 'mongodb';
 import python_code_data from './python.json'
 import ts_code_data from './ts.json'
+import { config } from '../../config';
 
 type codeData = {
                 id: number,
@@ -8,21 +9,21 @@ type codeData = {
                 url: string,
                 code: string,
 }
-
-const uri = 'http://localhost:27017';
-const client = new MongoClient(uri);
+;
+const client = new MongoClient(config.DB_URL);
 
 
 async function run() {
                 try {
                                 await client.connect();
-                                const database = client.db('zentyping') ? client.db('zentyping') : function () { throw new Error('Database not found') }();
-                                const python_collection = database.collection('python_code') ? database.collection('python_code') : function () { throw new Error('Collection not found') }();
-                                const ts_collection = database.collection('ts_code') ? database.collection('ts_code') : function () { throw new Error('Collection not found') }();
+                                 const database = client.db(config.DB_NAME) ? client.db(config.DB_NAME) : function () { throw new Error('Database not found') }();
+                                const python_collection = database.collection(config.PYTHON_COLLECTION_NAME) ? database.collection(config.PYTHON_COLLECTION_NAME) : function () { throw new Error('Collection not found') }();
+                                const ts_collection = database.collection(config.TS_COLLECTION_NAME) ? database.collection(config.TS_COLLECTION_NAME) : function () { throw new Error('Collection not found') }();
 
                                 if (ts_code_data.length === python_code_data.length) {
                                                 throw new Error('data length not equal')
                                 }
+                                
                                 const length = ts_code_data.length > python_code_data.length ? ts_code_data.length : python_code_data.length;
 
                                 for (let i = 0; i < length; i++) {
@@ -37,7 +38,7 @@ async function run() {
                                                 } catch (e) {
                                                                 console.error(e)
                                                                 return
-                                                }
+                                       }
                                 }
 
                 } finally {
